02b53ac7645bb97cb1e4db2e5d9c4757a65b8bef
erfan <erfan2ghorbani@gmail.com>
2022-08-08 21:02:57 +0430

    show post, and some options in index

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const Article = require('./models/article')
 const articleRouter = require('./routes/articles')
 const app = express()
 
@@ -10,21 +11,14 @@ mongoose.connect('mongodb://localhost/blog', {
 
 app.set('view engine', 'ejs')
 
-app.use('/articles', articleRouter)
+app.use(express.urlencoded({ extended : false }))
 
-app.get('/', (req, res) => {
-    const articles = [{
-        title : 'test article',
-        createdAt : new Date(),
-        description : 'test description'
-    },
-    {
-        title : 'test article 2',
-        createdAt : new Date(),
-        description : 'test description 2'
-    }]
+app.get('/', async (req, res) => {
+    const articles = await Article.find().sort({ createdAt : 'descending' })
     
     res.render('articles/index', { articles : articles })
 })
+
+app.use('/articles', articleRouter)
 
 app.listen(8000)
