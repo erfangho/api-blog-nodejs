278f8ea8a854501d6d0fb0878c095410eedf017a
erfan <erfan2ghorbani@gmail.com>
2022-08-08 20:18:41 +0430

    article routes, view, model

--- routes/articles.js
+++ routes/articles.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    res.send('in article routes')
+router.get('/new', (req, res) => {
+    res.render('articles/new')
+})
+
+router.post('/', (req, res) => {
+    
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const articleRouter = require('./routes/articles')
 const app = express()
+
+mongoose.connect('mongodb://localhost/blog', { 
+    useNewUrlParser : true,
+    useUnifiedTopology : true
+})
 
 app.set('view engine', 'ejs')
 
@@ -18,7 +24,7 @@ app.get('/', (req, res) => {
         description : 'test description 2'
     }]
     
-    res.render('index', { articles : articles })
+    res.render('articles/index', { articles : articles })
 })
 
 app.listen(8000)
